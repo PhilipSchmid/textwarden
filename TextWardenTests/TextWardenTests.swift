a482e49bd5b4fb0cf7d4038adb066767f94d2c2a
Philip Schmid <philip.schmid@protonmail.com>
2025-11-24 22:21:07 +0100

    added Swift unit tests
    Signed-off-by: Philip Schmid <philip.schmid@protonmail.com>

---
+++ TextWardenTests/TextWardenTests.swift
@@ -0,0 +1,16 @@
+//
+//  TextWardenTests.swift
+//  TextWardenTests
+//
+//  Created by Philip on 24.11.2025.
+//
+
+import Testing
+
+struct TextWardenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
